3f80214371125a5ed9873b25724587c6c737dce0
pauline-wim <linette.wim@hotmail.com>
2022-02-16 14:35:04 +0100

    fetch API movies

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,6 +1,25 @@
 import React from "react";
 
 class Popular extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7159e429659d95a41b81f7de34f57c58`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({ movies: res });
+      });
+  }
+
   render() {
     return (
       <div>
